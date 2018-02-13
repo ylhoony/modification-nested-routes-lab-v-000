020ce9b07eb41e18d7a766c2f971bf74ac3c7af0
Hoon Lee <yl.hoony@gmail.com>
2018-02-13 15:43:56 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,12 +1,14 @@
 module ArtistsHelper
   def display_artist(song)
-    # binding.pry
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
   def artist_select(artist, song)
-    binding.pry
+    if artist
+      artist.name
+    else
 
+    end
   end
 
 end
