cb3e0e615c4fdb9c2c073e1edd5e3f3315529e0a
Hoon Lee <yl.hoony@gmail.com>
2018-02-13 15:13:02 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -56,9 +56,12 @@ class SongsController < ApplicationController
     #   @post = Post.find(params[:id])
     # end
 
-    # if params[:artist_id]
-    #   artist = Artist.find_by(id: params[:artist_id])
-
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        @song = artist.songs.find_by(id: params[:id])
 
 binding.pry
 
