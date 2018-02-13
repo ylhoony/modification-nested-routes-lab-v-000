9bf0f43f4696358165a1d7bca5c792e11987e6fd
Hoon Lee <yl.hoony@gmail.com>
2018-02-13 15:22:03 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,11 +1,6 @@
 module SongsHelper
 
   def artist_id_field(song)
-    # if post.author.nil?
-    #   select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
-    # else
-    #   hidden_field_tag "post[author_id]", post.author_id
-    # end
     if song.artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
